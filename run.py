df54b71c0297ea37fb94a93f237cd9b587ceb3ea
Anh Vu Nguyen <avknguyen9@gmail.com>
2022-02-17 17:32:32 -0800

    1

--- run.py
+++ run.py
@@ -1,12 +1,10 @@
 from cProfile import label
 import numpy as np
-import matplotlib.pyplot as plt
 # An example dataset generator
 #from sklearn.datasets import make_blobs
 import csv
-from csv import reader
+from csv import reader, writer
 import pandas as pd
-import math
 import random
 from sklearn.datasets import make_classification
 
@@ -42,9 +40,11 @@ def run(Xtrain_file, Ytrain_file, test_data_file, pred_file):
     
     weighted_vec = [0]*3000
     learning_rate = .01
-    lamda = 1
+    lamda = .01
     LCL = 0
     while (LCL - (lamda * (np.linalg.norm(np.array(weighted_vec)) **2)) ) > .001  : 
+    
+    for x in range(5):
         shuffled = random.sample(data, len(data))
         for x in shuffled:
             #w = w + alpha( (y-p)x - 2(lamda * w))
@@ -59,14 +59,41 @@ def run(Xtrain_file, Ytrain_file, test_data_file, pred_file):
             difference = []
             for x in range(0, len(left)):
                 difference.append(left[x] - right[x])
-            difference = [element * lamda for element in difference]
-            #difference = (y-p)x - 2(lamda * w)
+            difference = [element * learning_rate for element in difference]
+            #difference = learning rate* ((y-p)x - 2(lamda * w))
             temp = []
             for x in range(len(difference)):
                 temp.append(weighted_vec[x] + difference[x])
             weighted_vec = temp
+        
+    accuracy = 0
+    #Prediction
+    with open(test_data_file) as f1, open(pred_file) as f2:
+        csv_reader = reader(f1)
+        csv_reader2 = writer(f2)
+        count = 0
+        for row in csv_reader:
+            int_map = map(int, row)
+            int_row = list(int_map)
+            prediction = sigmoid(np.dot(weighted_vec, int_row))
+            if prediction > .5:
+                csv_reader2.writerow(["0"])
+                if data[count][1] == 1:
+                    accuracy += 1
+            else:
+                csv_reader2.writerow(["1"])
+                if data[count][1] == 0:
+                    accuracy += 0
+            count += 1
+    print(accuracy/3000)
             
-    print(weighted_vec)
+
+
+
+
+
+
+
         
         
     
@@ -74,13 +101,13 @@ def run(Xtrain_file, Ytrain_file, test_data_file, pred_file):
   
 
 if __name__ == "__main__":
-    """ Xtrain_file = '../reference/Xtrain.csv'
+    Xtrain_file = '../reference/Xtrain.csv'
     Ytrain_file = '../reference/Ytrain.csv'
-    test_data_file = '../reference/Xtest.csv'  """
+    test_data_file = '../reference/Xtest.csv' 
 
-    Xtrain_file = 'Xtrain.csv'
+    """ Xtrain_file = 'Xtrain.csv'
     Ytrain_file = 'Ytrain.csv'
-    test_data_file = 'Xtest.csv' 
+    test_data_file = 'Xtrain.csv'  """
 
 
     pred_file = 'predictions.csv'
